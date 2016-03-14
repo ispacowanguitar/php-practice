27f716389539ac4d7074d3a7cbfbbf6573c75714
ispacowanguitar <ispacowanguitar@gmail.com>
2016-03-14 16:54:19 -0500

    added working child class, which extends person

--- people.php
+++ people.php
@@ -6,15 +6,44 @@ class Person
   public $name = "Default Name";
   public $age = 0;
 
+  public function __construct($personProperties)
+  {
+    $this->name = $personProperties['name'];
+    $this->age = $personProperties['age'];
+  }
+
   public function Describe() 
   {
     return $this->name . " is " . $this->age . " years old \n";
   }
+
+  public function Scream()
+  {
+    return $this->name . " Says AAAAAAARG";
+  }
 }
 
-$alex = new Person;
-$alex->name = "Alex Ispa-Cowan";
-$alex->age = 27;
-echo $alex->Describe();
+Class Child extends Person
+{
+  public $favoriteColor;
+
+  public function __construct($personProperties)
+  {
+    $this->name = $personProperties['name'];
+    $this->age = $personProperties['age'];
+  }
+}
+
+// $personProperties = array('name' => 'Alex', 'age' => 27);
+$alex = new Person(array('name' => 'Alex', 'age' => 27));
+// $alex->name = "Alex Ispa-Cowan";
+// $alex->age = 27;
+
+$isaac = new Child(array('name' => 'Isaac', 'age' => 1));
+$isaac->favoriteColor = "Blue";
+
+
+echo $isaac->Describe();
+echo $isaac->Scream() . "\n";
 
 ?>
